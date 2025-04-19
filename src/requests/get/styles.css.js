569b6220b88e010e2a5e7ffe388ed4d75d031fd8
James Miller <JMTNTBANG@proton.me>
2025-04-18 23:30:21 -0500

    v0.4.0 - Write API - #17 #18
    Also Moved HTML, CSS, and JS code to sister project

--- src/requests/get/styles.css.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  init: (prefix, website) => {
-    website.get(`${prefix}styles.css`, (request, response) => {
-      response.sendFile(`${__dirname.slice(0, -13)}/static/styles.css`);
-    });
-  },
-};
