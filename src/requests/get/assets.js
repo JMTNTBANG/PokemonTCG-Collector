c28d19e50898cb3470a1cdcfa1d4377af98dd3c8
James Miller <JMTNTBANG@proton.me>
2025-09-20 16:46:56 -0500

    Cleanup from Old Version

--- src/requests/get/assets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    init: (prefix, website) => {
-      website.get(`${prefix}assets`, (request, response) => {
-        response.sendFile(`${__dirname.slice(0, -13)}/assets/${request.query.id}.png`);
-      });
-    },
-  };
-  
